3ad0d132c6e1a8c03eaaa9ff68663b05b42066d3
Scott Williams <scottcolemanwilliams@gmail.com>
2019-05-21 16:57:36 -0500

    api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  //API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://gentle-wildwood-77962.herokuapp.com/api',
   TOKEN_KEY: 'carpe-duo-client-auth-token',
 }
